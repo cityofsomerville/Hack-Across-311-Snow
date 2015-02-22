afc3f3b67017bb5e792d71f656fd56c6986d1123
DanielHadley <dphnrome@gmail.com>
2015-02-22 11:43:01 -0500

    d3 bar chart of the top municipalities

--- charts/d3Snow.R
+++ charts/d3Snow.R
@@ -32,22 +32,6 @@ d <- snowData %>%
          year = year(date)) 
 
 
-#### Visualize ####
-
-my.theme <- 
-  theme(#plot.background = element_rect(fill="white"), # Remove background
-    panel.grid.major = element_blank(), # Remove gridlines
-    # panel.grid.minor = element_blank(), # Remove more gridlines
-    # panel.border = element_blank(), # Remove border
-    # panel.background = element_blank(), # Remove more background
-    axis.ticks = element_blank(), # Remove axis ticks
-    axis.text=element_text(size=6), # Enlarge axis text font
-    axis.title=element_text(size=8), # Enlarge axis title font
-    plot.title=element_text(size=12) # Enlarge, left-align title
-    ,axis.text.x = element_text(angle=60, hjust = 1) # Uncomment if X-axis unreadable 
-  )
-
-
 ### This will give me a timeseries of every day
 days <- d %>%
   group_by(date) %>%
@@ -57,35 +41,38 @@ allDays <- seq.Date(from=d$date[1], to = d$date[nrow(d)], b='days')
 allDays <- allDays  %>%  as.data.frame() 
 colnames(allDays)[1] = "date"
 
-# After this we will have a df with every date and how many BnEs
+# After this we will have a df with every date and how many snow issues
 ts = merge(days, allDays, by='date', all=TRUE)
 ts[is.na(ts)] <- 0
 
 remove(allDays, days)
 ts$id <- seq(1, nrow(ts))
 
-ggplot(ts, aes(x=date, y=Events, group = 1)) + 
-  geom_line(colour='red', size = 1) + 
-  my.theme + ggtitle("Daily Snow Issues") + xlab("Year") +
-  ylab("311 Snow Issues") + 
-  scale_y_continuous(labels = comma) +
-  scale_x_date(labels=date_format("%Y"))
 
-
+#### Now make the d3 chart using rCharts ####
 cumulative <- ts %>%
-  mutate(cumulativeIssues = cumsum(Events))
+  mutate(Snow.Issues = cumsum(Events))
 
-
-ggplot(cumulativeIssues, aes(x=date, y=cumulative, group = 1)) + 
-  geom_line(colour='red', size = 1) 
-
-## Now make the d3 chart using rCharts
-
-d3chart<-nPlot(cumulativeIssues~date, data=cumulative, type="lineChart")
+d3chart<-nPlot(Snow.Issues~date, data=cumulative, type="lineChart")
 
 d3chart$xAxis(
   tickFormat = "#!function(d) {return d3.time.format('%b %Y')(new Date( d * 86400000 ));}!#"
 )
 
+## Save it
+
+d3chart$save('d3chart.html', cdn=TRUE)
 
 
+#### Now a bar chart of top cities/towns ####
+
+byMuni <- d %>%
+  group_by(city) %>%
+  summarise(Snow.Issues = n()) %>%
+  filter(Snow.Issues > 100)
+  
+
+d3chart2 <- nPlot(Snow.Issues ~ city, data = byMuni, type = "multiBarChart")
+
+## Save it
+d3chart2$save('d3chart2.html', cdn=TRUE)
